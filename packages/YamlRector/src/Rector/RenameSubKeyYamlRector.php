368fbb978eb158f1b9b74d255bf49557048c0cd6
Tomas Votruba <tomas.vot@gmail.com>
2018-06-29 18:00:54 +0200

    cleanup

--- packages/YamlRector/src/Rector/RenameSubKeyYamlRector.php
+++ packages/YamlRector/src/Rector/RenameSubKeyYamlRector.php
@@ -23,8 +23,7 @@ final class RenameSubKeyYamlRector implements YamlRectorInterface
     public function isCandidate(string $content): bool
     {
         foreach ($this->pathsToNewKeys as $path => $newKey) {
-            $pathSteps = Strings::split($path, '#[\s+]?>[\s+]?#');
-            $pathPattern = $this->createPattern($pathSteps);
+            $pathPattern = $this->createPatternFromPath($path);
 
             if ((bool) Strings::match($content, $pathPattern)) {
                 return true;
@@ -37,11 +36,10 @@ final class RenameSubKeyYamlRector implements YamlRectorInterface
     public function refactor(string $content): string
     {
         foreach ($this->pathsToNewKeys as $path => $newKey) {
-            $pathSteps = Strings::split($path, '#[\s+]?>[\s+]?#');
-            $pathPattern = $this->createPattern($pathSteps);
+            $pathPattern = $this->createPatternFromPath($path);
 
             while (Strings::match($content, $pathPattern)) {
-                $replacement = $this->createReplacement($pathSteps, $newKey);
+                $replacement = $this->createReplacementFromPathAndNewKey($path, $newKey);
                 $content = Strings::replace($content, $pathPattern, $replacement, 1);
             }
         }
@@ -49,19 +47,18 @@ final class RenameSubKeyYamlRector implements YamlRectorInterface
         return $content;
     }
 
-    /**
-     * @param string[] $pathSteps
-     */
-    private function createPattern(array $pathSteps): string
+    private function createPatternFromPath(string $path): string
     {
+        $pathParts = Strings::split($path, '#[\s+]?>[\s+]?#');
+
         $pattern = '';
-        foreach ($pathSteps as $key => $pathStep) {
-            if ($key === (count($pathSteps) - 1)) {
+        foreach ($pathParts as $key => $pathPart) {
+            if ($key === (count($pathParts) - 1)) {
                 // last only up-to the key name
-                $pattern .= sprintf('(%s)(.*?)', preg_quote($pathStep));
+                $pattern .= sprintf('(%s)(.*?)', preg_quote($pathPart));
             } else {
                 // see https://regex101.com/r/n8XPbV/3/ for ([^:]*?)
-                $pattern .= sprintf('(%s:\s+)([^:]*?)', preg_quote($pathStep));
+                $pattern .= sprintf('(%s:\s+)([^:]*?)', preg_quote($pathPart));
             }
         }
 
@@ -71,11 +68,13 @@ final class RenameSubKeyYamlRector implements YamlRectorInterface
     /**
      * @param string[] $pathSteps
      */
-    private function createReplacement(array $pathSteps, string $newKey): string
+    private function createReplacementFromPathAndNewKey(string $path, string $newKey): string
     {
         $replacement = '';
 
-        $final = 2 * count($pathSteps);
+        $pathParts = Strings::split($path, '#[\s+]?>[\s+]?#');
+
+        $final = 2 * count($pathParts);
         for ($i = 1; $i < $final - 1; ++$i) {
             $replacement .= '$' . $i;
         }
